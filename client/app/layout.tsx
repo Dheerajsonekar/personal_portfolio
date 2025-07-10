6f1ae9dffd16bc6bc69ea60f1e2063deb125a178
Dheerajsonekar <dheerajsonekar2@gmail.com>
2025-07-10 15:52:16 +0530

    change font

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,21 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 import { ReactNode } from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-  display: "swap",
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-  display: "swap",
-});
 
 export const metadata: Metadata = {
   title: "Dheeraj Sonekar | Full Stack Developer Portfolio",
@@ -24,7 +14,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" >
       <body className="min-h-screen flex flex-col font-sans">
         <Navbar />
         <main>
